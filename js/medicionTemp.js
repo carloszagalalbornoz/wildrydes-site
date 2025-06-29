80fc9c069a5c9465642f55a7ea367add9e258ad6
carloszagalalbornoz <cr.zagal@gmail.com>
2025-06-28 20:22:48 -0400

    Update medicionTemp.js

--- js/medicionTemp.js
+++ js/medicionTemp.js
@@ -1,1 +1,93 @@
+/*global WildRydes _config AmazonCognitoIdentity AWSCognito*/
 
+var WildRydes = window.WildRydes || {};
+
+(function scopeWrapper($) {
+    var signinUrl = '/signin.html';
+
+    var poolData = {
+        UserPoolId: _config.cognito.userPoolId,
+        ClientId: _config.cognito.userPoolClientId
+    };
+
+    var userPool;
+
+    if (!(_config.cognito.userPoolId &&
+          _config.cognito.userPoolClientId &&
+          _config.cognito.region)) {
+        $('#noCognitoMessage').show();
+        return;
+    }
+
+    userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
+
+    if (typeof AWSCognito !== 'undefined') {
+        AWSCognito.config.region = _config.cognito.region;
+    }
+
+    WildRydes.signOut = function signOut() {
+        userPool.getCurrentUser().signOut();
+    };
+
+    WildRydes.authToken = new Promise(function fetchCurrentAuthToken(resolve, reject) {
+        var cognitoUser = userPool.getCurrentUser();
+
+        if (cognitoUser) {
+            cognitoUser.getSession(function sessionCallback(err, session) {
+                if (err) {
+                    reject(err);
+                } else if (!session.isValid()) {
+                    resolve(null);
+                } else {
+                    resolve(session.getIdToken().getJwtToken());
+                }
+            });
+        } else {
+            resolve(null);
+        }
+    });
+
+
+    /*
+     * Cognito User Pool functions
+     */
+    function toUsername(email) {
+        return email.replace('@', '-at-');
+    }
+
+    /*
+     *  Event Handlers
+     */
+
+    $(function onDocReady() {
+
+        $('#registrationForm').submit(handleRegister);
+
+    });
+
+    function handleRegister(event) {
+        var email = $('#emailInputRegister').val();
+        var password = $('#passwordInputRegister').val();
+        var password2 = $('#password2InputRegister').val();
+console.log("email");
+        var onSuccess = function registerSuccess(result) {
+            var cognitoUser = result.user;
+            console.log('user name is ' + cognitoUser.getUsername());
+            var confirmation = ('Registration successful. Please check your email inbox or spam folder for your verification code.');
+            if (confirmation) {
+                window.location.href = 'verify.html';
+            }
+        };
+        var onFailure = function registerFailure(err) {
+            alert(err);
+        };
+        event.preventDefault();
+
+        if (password === password2) {
+            register(email, password, onSuccess, onFailure);
+        } else {
+            alert('Passwords do not match');
+        }
+    }
+
+}(jQuery));
